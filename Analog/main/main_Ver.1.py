7421890f54afef2eac8d019df8d1f58470fbebb6
aegis-katoh <aegis.katoh@gmail.com>
2017-12-01 17:52:05 +0900

    Complete Ver.1

--- Analog/main/main_Ver.1.py
+++ Analog/main/main_Ver.1.py
@@ -1,7 +1,7 @@
 # This program is written in Python3
 # Author : Masazumi Katoh
 # coding : UTF-8
-# Last Update : 2017/11/30
+# Last Update : 2017/12/1
 
 # to use MCP3008
 from gpiozero import MCP3008
