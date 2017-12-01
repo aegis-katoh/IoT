e695f71a8067babad7613d9b196e8ae3b147f57b
aegis-katoh <aegis.katoh@gmail.com>
2017-12-01 16:32:09 +0900

    no message

--- Analog/main/get_sensor_value_debug.py
+++ Analog/main/get_sensor_value_debug.py
@@ -1,14 +1,25 @@
 # This program is written in Python3
 
 from goipzero import MCP3008
+from datetime import datetime
 
 def get_sensor_value(channel, value_list, length):
+	t1 = datetime.now()
 	input_value = MCP3008(channel = channel)
+	t2 = datetime.now()
 	present_value = V0 * input_value.value
+	t3 = datetime.now()
 
 	for i in range(length - 1):
 		value_list[i] = value_list[i+1]
 
+	t4 = datetime.now()
 	value_list[length - 1] = present_value
+	t5 = datetime.now()
+
+	print("input :", t2 - t1)
+	print("voltage :", t3 - t2)
+	print("for loop :", t4 - t3)
+	print("present :", t5 - t4)
 
 	return value_list
